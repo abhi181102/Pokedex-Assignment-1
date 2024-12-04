042e83cc3799f1382dba642bbdb1e1f96bbb9163
abhi181102 <abhi69432@gmail.com>
2024-12-03 19:42:18 -0500

    "Added event listener to 'getWeather' button to log user-input location to console"

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("getWeather").addEventListener("click", () => {
+  const location = document.getElementById("location").value.trim();
+  console.log("City entered:", location);
+});
